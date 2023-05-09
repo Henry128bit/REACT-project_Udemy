20e50bd5ba538e870fab3fc7fca92c3734201450
Sergey Kamensky <henry.128bit@gmail.com>
2023-05-09 12:46:51 +0400

    Divide the application into components. Made layout.

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,13 @@
+import './search-panel.css';
+
+const SeacrhPanel = () => {
+    return (
+        <input 
+            type="text"
+            className="form-control search-input"
+            placeholder="Find an employee"
+        />
+    )
+}
+
+export default SeacrhPanel;
